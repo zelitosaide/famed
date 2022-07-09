8f4ee7575478f2316d151444622cf8337a12ecbb
zelitosaide <42151635+zelitosaide@users.noreply.github.com>
2022-07-09 15:39:38 +0200

    Update client.js

--- src/api/client.js
+++ src/api/client.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
-const API = axios.create({ baseURL: 'http://localhost:5000' })
-// const API = axios.create({ baseURL: 'https://famed-v1.herokuapp.com/' })
+// const API = axios.create({ baseURL: 'http://localhost:5000' })
+const API = axios.create({ baseURL: 'https://famed-v1.herokuapp.com/' })
 
 API.interceptors.request.use((req) => {
   if (localStorage.getItem('famedv1_user')) {
