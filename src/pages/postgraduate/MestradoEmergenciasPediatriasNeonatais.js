0daed2cf843b5c494b0ed03599760ef5622797ce
zelitosaide <zelitosaide83@gmail.com>
2022-11-22 15:17:18 +0200

    postgraduate courses

--- src/pages/postgraduate/MestradoEmergenciasPediatriasNeonatais.js
+++ src/pages/postgraduate/MestradoEmergenciasPediatriasNeonatais.js
@@ -35,10 +35,6 @@ export function MestradoEmergenciasPediatriasNeonatais() {
           O MEPN visa formar profissionais de saúde para providenciar cuidados de saúde, e realizarem docência e pesquisas relevantes, na área de pediatria e neonatologia, para serem capazes de darem o seu melhor contributo no contexto de equipas multidisciplinares. Os estudantes que completarem este curso com sucesso receberão o grau de <i>Mestre em Emergências Pediatrias e Neonatais</i>.
         </p>
 
-
-
-
-
         <p
           style={{
             fontSize: "0.92rem",
@@ -69,28 +65,14 @@ export function MestradoEmergenciasPediatriasNeonatais() {
           <strong><i>O início do curso</i></strong> está previsto para Agosto de 2022.
         </p>
 
-
-        <p
-          style={{
-            fontSize: "0.92rem",
-            fontWeight: "bold",
-            color: "var(--main-color)",
-            lineHeight: "1.2rem",
-            marginTop: "1.5rem"
-          }}
-        >
-          Vagas:
-        </p>
-
         <p
           style={{
             lineHeight: "1.6rem",
             fontSize: "0.875rem",
           }}
         >
-          O número máximo de vagas estimado para este curso é de 30. O curso só abrirá se tiver um número mínimo de 15 estudantes.
+          <strong><i>O número máximo de vagas</i></strong> estimado para este curso é de 30. O curso só abrirá se tiver um número mínimo de 15 estudantes.
         </p>
-
 
         <p
           style={{
@@ -104,18 +86,18 @@ export function MestradoEmergenciasPediatriasNeonatais() {
           Grupo alvo:
         </p>
 
-        <ul style={{ listStyle: "circle", marginLeft: "1.5rem" }}>
+        <ul style={{ listStyle: "disc", marginLeft: "1.5rem" }}>
           <li
             style={{
               lineHeight: "1.6rem",
               fontSize: "0.875rem",
               // fontWeight: 500,
               marginBottom: 10,
-              textDecoration: "underline",
-              textUnderlineOffset: 3,
-              textDecorationThickness: 1.5,
+              // textDecoration: "underline",
+              // textUnderlineOffset: 3,
+              // textDecorationThickness: 1.5,
               // textDecorationColor: "var(--main-color)",
-              textDecorationColor: "var(--hover-color)",
+              // textDecorationColor: "var(--hover-color)",
             }}
           >
             Ter Licenciatura em Medicina; Especialidade de Pediatria; Curso Superior de Enfermagem Geral; Curso Superior de Enfermagem Pediátrica; Curso Superior de Enfermagem em Saúde Materna; Curso Superior de Técnico de Cirurgia e Obstetrícia, ou outras afins;
@@ -126,11 +108,11 @@ export function MestradoEmergenciasPediatriasNeonatais() {
               fontSize: "0.875rem",
               // fontWeight: 500,
               marginBottom: 10,
-              textDecoration: "underline",
-              textUnderlineOffset: 3,
-              textDecorationThickness: 1.5,
+              // textDecoration: "underline",
+              // textUnderlineOffset: 3,
+              // textDecorationThickness: 1.5,
               // textDecorationColor: "var(--main-color)",
-              textDecorationColor: "var(--hover-color)",
+              // textDecorationColor: "var(--hover-color)",
             }}
           >
             Possuir Grau de Licenciatura com nota de conclusão igual ou superior a 14 valores;
@@ -142,11 +124,11 @@ export function MestradoEmergenciasPediatriasNeonatais() {
               fontSize: "0.875rem",
               // fontWeight: 500,
               marginBottom: 10,
-              textDecoration: "underline",
-              textUnderlineOffset: 3,
-              textDecorationThickness: 1.5,
+              // textDecoration: "underline",
+              // textUnderlineOffset: 3,
+              // textDecorationThickness: 1.5,
               // textDecorationColor: "var(--main-color)",
-              textDecorationColor: "var(--hover-color)",
+              // textDecorationColor: "var(--hover-color)",
             }}
           >
             Excepcionalmente, poderão ingressar os candidatos com grau de Licenciatura com média final não inferior a 12 valores, por questões de mérito (currículo científico relevante, experiência de trabalho comprovada na área de conhecimento do Mestrado, ter publicações científicas, ser capaz de fundamentar a investigação que pretende realizar);
@@ -158,11 +140,11 @@ export function MestradoEmergenciasPediatriasNeonatais() {
               fontSize: "0.875rem",
               // fontWeight: 500,
               marginBottom: 10,
-              textDecoration: "underline",
-              textUnderlineOffset: 3,
-              textDecorationThickness: 1.5,
+              // textDecoration: "underline",
+              // textUnderlineOffset: 3,
+              // textDecorationThickness: 1.5,
               // textDecorationColor: "var(--main-color)",
-              textDecorationColor: "var(--hover-color)",
+              // textDecorationColor: "var(--hover-color)",
             }}
           >
             Ter domínio da língua portuguesa;
@@ -174,11 +156,11 @@ export function MestradoEmergenciasPediatriasNeonatais() {
               fontSize: "0.875rem",
               // fontWeight: 500,
               marginBottom: 10,
-              textDecoration: "underline",
-              textUnderlineOffset: 3,
-              textDecorationThickness: 1.5,
+              // textDecoration: "underline",
+              // textUnderlineOffset: 3,
+              // textDecorationThickness: 1.5,
               // textDecorationColor: "var(--main-color)",
-              textDecorationColor: "var(--hover-color)",
+              // textDecorationColor: "var(--hover-color)",
             }}
           >
             Ter conhecimentos de língua inglesa é uma vantagem;
@@ -190,11 +172,11 @@ export function MestradoEmergenciasPediatriasNeonatais() {
               fontSize: "0.875rem",
               // fontWeight: 500,
               marginBottom: 10,
-              textDecoration: "underline",
-              textUnderlineOffset: 3,
-              textDecorationThickness: 1.5,
+              // textDecoration: "underline",
+              // textUnderlineOffset: 3,
+              // textDecorationThickness: 1.5,
               // textDecorationColor: "var(--main-color)",
-              textDecorationColor: "var(--hover-color)",
+              // textDecorationColor: "var(--hover-color)",
             }}
           >
             Os candidatos pré-selecionados serão submetidos a uma entrevista a ser realizada na Faculdade de Medicina da UEM em data e horários a serem anunciados;
@@ -206,11 +188,11 @@ export function MestradoEmergenciasPediatriasNeonatais() {
               fontSize: "0.875rem",
               // fontWeight: 500,
               marginBottom: 10,
-              textDecoration: "underline",
-              textUnderlineOffset: 3,
-              textDecorationThickness: 1.5,
+              // textDecoration: "underline",
+              // textUnderlineOffset: 3,
+              // textDecorationThickness: 1.5,
               // textDecorationColor: "var(--main-color)",
-              textDecorationColor: "var(--hover-color)",
+              // textDecorationColor: "var(--hover-color)",
             }}
           >
             Encoraja-se candidaturas do sexo feminino.
@@ -229,7 +211,6 @@ export function MestradoEmergenciasPediatriasNeonatais() {
           Procedimentos para candidatura:
         </p>
 
-
         <p
           style={{
             lineHeight: "1.6rem",
@@ -238,7 +219,6 @@ export function MestradoEmergenciasPediatriasNeonatais() {
         >
           Requer-se que os candidatos apresentem os seguintes documentos na Direcção da Faculdade de Medicina da UEM:
         </p>
-
 
         <ul style={{ listStyle: "circle", marginLeft: "1.5rem" }}>
           <li
@@ -369,7 +349,6 @@ export function MestradoEmergenciasPediatriasNeonatais() {
           </li>
         </ul>
 
-
         <p
           style={{
             fontSize: "0.92rem",
@@ -391,8 +370,6 @@ export function MestradoEmergenciasPediatriasNeonatais() {
           15 de Julho de 2022
         </p>
 
-
-
         <p
           style={{
             fontSize: "0.92rem",
@@ -413,8 +390,6 @@ export function MestradoEmergenciasPediatriasNeonatais() {
         >
           A taxa de matrícula anual é de 1.750,00 Mt; Valor de inscrição 9.000,00 Mt. A propina de frequência é de 9.000,00 Mt por mês pagos na íntegra ou em prestações mensais durante 24 meses.
         </p>
-
-
 
         <p
           style={{
--- src/pages/postgraduate/MestradoSaudeMentalPsicoIntervencoes.js
+++ src/pages/postgraduate/MestradoSaudeMentalPsicoIntervencoes.js
@@ -13,6 +13,404 @@ export function MestradoSaudeMentalPsicoIntervencoes() {
         >
           Mestrado em Saúde Mental e Psico-intervenções (MSMPI)
         </p>
+
+        <p
+          style={{
+            fontSize: "0.92rem",
+            fontWeight: "bold",
+            color: "var(--main-color)",
+            lineHeight: "1.2rem",
+            marginTop: "1.5rem"
+          }}
+        >
+          Objectivo geral do Programa:
+        </p>
+
+        <p
+          style={{
+            lineHeight: "1.6rem",
+            fontSize: "0.875rem",
+          }}
+        >
+          O MEPN visa formar profissionais de saúde para providenciar cuidados de saúde, e realizarem docência e pesquisas relevantes, na área de pediatria e neonatologia, para serem capazes de darem o seu melhor contributo no contexto de equipas multidisciplinares. Os estudantes que completarem este curso com sucesso receberão o grau de <i>Mestre em Emergências Pediatrias e Neonatais</i>.
+        </p>
+
+        <p
+          style={{
+            fontSize: "0.92rem",
+            fontWeight: "bold",
+            color: "var(--main-color)",
+            lineHeight: "1.2rem",
+            marginTop: "1.5rem"
+          }}
+        >
+          Organização do Programa:
+        </p>
+
+        <p
+          style={{
+            lineHeight: "1.6rem",
+            fontSize: "0.875rem",
+          }}
+        >
+          As aulas do MEPN decorrerão essencialmente na Faculdade de Medicina da UEM e no Departamento de Pediatria do Hospital Central de Maputo (HCM), <strong><i>no período laboral</i></strong>. Os trabalhos de culminação de estudos irão decorrer nas diferentes instituições de ensino envolvidas repectivamente: UEM, UNIPD, UCM, HCM, Hospital Central da Beira (HCB) ou em outras instituições de pesquisa parceiras da Faculdade de Medicina da UEM ou do Serviço Nacional de Saúde.
+        </p>
+
+        <p
+          style={{
+            lineHeight: "1.6rem",
+            fontSize: "0.875rem",
+          }}
+        >
+          <strong><i>O início do curso</i></strong> está previsto para Agosto de 2022.
+        </p>
+
+        <p
+          style={{
+            lineHeight: "1.6rem",
+            fontSize: "0.875rem",
+          }}
+        >
+          <strong><i>O número máximo de vagas</i></strong> estimado para este curso é de 30. O curso só abrirá se tiver um número mínimo de 15 estudantes.
+        </p>
+
+        <p
+          style={{
+            fontSize: "0.92rem",
+            fontWeight: "bold",
+            color: "var(--main-color)",
+            lineHeight: "1.2rem",
+            marginTop: "1.5rem"
+          }}
+        >
+          Grupo alvo:
+        </p>
+
+        <ul style={{ listStyle: "disc", marginLeft: "1.5rem" }}>
+          <li
+            style={{
+              lineHeight: "1.6rem",
+              fontSize: "0.875rem",
+              // fontWeight: 500,
+              marginBottom: 10,
+              // textDecoration: "underline",
+              // textUnderlineOffset: 3,
+              // textDecorationThickness: 1.5,
+              // textDecorationColor: "var(--main-color)",
+              // textDecorationColor: "var(--hover-color)",
+            }}
+          >
+            Ter Licenciatura em Medicina; Especialidade de Pediatria; Curso Superior de Enfermagem Geral; Curso Superior de Enfermagem Pediátrica; Curso Superior de Enfermagem em Saúde Materna; Curso Superior de Técnico de Cirurgia e Obstetrícia, ou outras afins;
+          </li>
+          <li
+            style={{
+              lineHeight: "1.6rem",
+              fontSize: "0.875rem",
+              // fontWeight: 500,
+              marginBottom: 10,
+              // textDecoration: "underline",
+              // textUnderlineOffset: 3,
+              // textDecorationThickness: 1.5,
+              // textDecorationColor: "var(--main-color)",
+              // textDecorationColor: "var(--hover-color)",
+            }}
+          >
+            Possuir Grau de Licenciatura com nota de conclusão igual ou superior a 14 valores;
+          </li>
+
+          <li
+            style={{
+              lineHeight: "1.6rem",
+              fontSize: "0.875rem",
+              // fontWeight: 500,
+              marginBottom: 10,
+              // textDecoration: "underline",
+              // textUnderlineOffset: 3,
+              // textDecorationThickness: 1.5,
+              // textDecorationColor: "var(--main-color)",
+              // textDecorationColor: "var(--hover-color)",
+            }}
+          >
+            Excepcionalmente, poderão ingressar os candidatos com grau de Licenciatura com média final não inferior a 12 valores, por questões de mérito (currículo científico relevante, experiência de trabalho comprovada na área de conhecimento do Mestrado, ter publicações científicas, ser capaz de fundamentar a investigação que pretende realizar);
+          </li>
+
+          <li
+            style={{
+              lineHeight: "1.6rem",
+              fontSize: "0.875rem",
+              // fontWeight: 500,
+              marginBottom: 10,
+              // textDecoration: "underline",
+              // textUnderlineOffset: 3,
+              // textDecorationThickness: 1.5,
+              // textDecorationColor: "var(--main-color)",
+              // textDecorationColor: "var(--hover-color)",
+            }}
+          >
+            Ter domínio da língua portuguesa;
+          </li>
+
+          <li
+            style={{
+              lineHeight: "1.6rem",
+              fontSize: "0.875rem",
+              // fontWeight: 500,
+              marginBottom: 10,
+              // textDecoration: "underline",
+              // textUnderlineOffset: 3,
+              // textDecorationThickness: 1.5,
+              // textDecorationColor: "var(--main-color)",
+              // textDecorationColor: "var(--hover-color)",
+            }}
+          >
+            Ter conhecimentos de língua inglesa é uma vantagem;
+          </li>
+
+          <li
+            style={{
+              lineHeight: "1.6rem",
+              fontSize: "0.875rem",
+              // fontWeight: 500,
+              marginBottom: 10,
+              // textDecoration: "underline",
+              // textUnderlineOffset: 3,
+              // textDecorationThickness: 1.5,
+              // textDecorationColor: "var(--main-color)",
+              // textDecorationColor: "var(--hover-color)",
+            }}
+          >
+            Os candidatos pré-selecionados serão submetidos a uma entrevista a ser realizada na Faculdade de Medicina da UEM em data e horários a serem anunciados;
+          </li>
+
+          <li
+            style={{
+              lineHeight: "1.6rem",
+              fontSize: "0.875rem",
+              // fontWeight: 500,
+              marginBottom: 10,
+              // textDecoration: "underline",
+              // textUnderlineOffset: 3,
+              // textDecorationThickness: 1.5,
+              // textDecorationColor: "var(--main-color)",
+              // textDecorationColor: "var(--hover-color)",
+            }}
+          >
+            Encoraja-se candidaturas do sexo feminino.
+          </li>
+        </ul>
+
+        <p
+          style={{
+            fontSize: "0.92rem",
+            fontWeight: "bold",
+            color: "var(--main-color)",
+            lineHeight: "1.2rem",
+            marginTop: "1.5rem"
+          }}
+        >
+          Procedimentos para candidatura:
+        </p>
+
+        <p
+          style={{
+            lineHeight: "1.6rem",
+            fontSize: "0.875rem",
+          }}
+        >
+          Requer-se que os candidatos apresentem os seguintes documentos na Direcção da Faculdade de Medicina da UEM:
+        </p>
+
+        <ul style={{ listStyle: "circle", marginLeft: "1.5rem" }}>
+          <li
+            style={{
+              lineHeight: "1.6rem",
+              fontSize: "0.875rem",
+              // fontWeight: 500,
+              marginBottom: 10,
+              textDecoration: "underline",
+              textUnderlineOffset: 3,
+              textDecorationThickness: 1.5,
+              // textDecorationColor: "var(--main-color)",
+              textDecorationColor: "var(--hover-color)",
+            }}
+          >
+            Carta endereçada ao Senhor Director da Faculdade de Medicina da UEM, manifestando o interesse em candidatar-se;
+          </li>
+          <li
+            style={{
+              lineHeight: "1.6rem",
+              fontSize: "0.875rem",
+              // fontWeight: 500,
+              marginBottom: 10,
+              textDecoration: "underline",
+              textUnderlineOffset: 3,
+              textDecorationThickness: 1.5,
+              // textDecorationColor: "var(--main-color)",
+              textDecorationColor: "var(--hover-color)",
+            }}
+          >
+            Carta de Motivação;
+          </li>
+
+          <li
+            style={{
+              lineHeight: "1.6rem",
+              fontSize: "0.875rem",
+              // fontWeight: 500,
+              marginBottom: 10,
+              textDecoration: "underline",
+              textUnderlineOffset: 3,
+              textDecorationThickness: 1.5,
+              // textDecorationColor: "var(--main-color)",
+              textDecorationColor: "var(--hover-color)",
+            }}
+          >
+            Curriculum Vitae;
+          </li>
+
+          <li
+            style={{
+              lineHeight: "1.6rem",
+              fontSize: "0.875rem",
+              // fontWeight: 500,
+              marginBottom: 10,
+              textDecoration: "underline",
+              textUnderlineOffset: 3,
+              textDecorationThickness: 1.5,
+              // textDecorationColor: "var(--main-color)",
+              textDecorationColor: "var(--hover-color)",
+            }}
+          >
+            Cópia autenticada do Certificado de Licenciatura;
+          </li>
+
+          <li
+            style={{
+              lineHeight: "1.6rem",
+              fontSize: "0.875rem",
+              // fontWeight: 500,
+              marginBottom: 10,
+              textDecoration: "underline",
+              textUnderlineOffset: 3,
+              textDecorationThickness: 1.5,
+              // textDecorationColor: "var(--main-color)",
+              textDecorationColor: "var(--hover-color)",
+            }}
+          >
+            Cópia autenticada do Bilhete de Identificação;
+          </li>
+
+          <li
+            style={{
+              lineHeight: "1.6rem",
+              fontSize: "0.875rem",
+              // fontWeight: 500,
+              marginBottom: 10,
+              textDecoration: "underline",
+              textUnderlineOffset: 3,
+              textDecorationThickness: 1.5,
+              // textDecorationColor: "var(--main-color)",
+              textDecorationColor: "var(--hover-color)",
+            }}
+          >
+            Duas cartas de recomendação;
+          </li>
+
+          <li
+            style={{
+              lineHeight: "1.6rem",
+              fontSize: "0.875rem",
+              // fontWeight: 500,
+              marginBottom: 10,
+              textDecoration: "underline",
+              textUnderlineOffset: 3,
+              textDecorationThickness: 1.5,
+              // textDecorationColor: "var(--main-color)",
+              textDecorationColor: "var(--hover-color)",
+            }}
+          >
+            Duas fotografias tipo passe recentes;
+          </li>
+
+          <li
+            style={{
+              lineHeight: "1.6rem",
+              fontSize: "0.875rem",
+              // fontWeight: 500,
+              marginBottom: 10,
+              textDecoration: "underline",
+              textUnderlineOffset: 3,
+              textDecorationThickness: 1.5,
+              // textDecorationColor: "var(--main-color)",
+              textDecorationColor: "var(--hover-color)",
+            }}
+          >
+            Carta de autorização do serviço (para o candidato trabalhador).
+          </li>
+        </ul>
+
+        <p
+          style={{
+            fontSize: "0.92rem",
+            fontWeight: "bold",
+            color: "var(--main-color)",
+            lineHeight: "1.2rem",
+            marginTop: "1.5rem"
+          }}
+        >
+          Data Limite da Submissão de Candidaturas:
+        </p>
+
+        <p
+          style={{
+            lineHeight: "1.6rem",
+            fontSize: "0.875rem",
+          }}
+        >
+          15 de Julho de 2022
+        </p>
+
+        <p
+          style={{
+            fontSize: "0.92rem",
+            fontWeight: "bold",
+            color: "var(--main-color)",
+            lineHeight: "1.2rem",
+            marginTop: "1.5rem"
+          }}
+        >
+          Propinas e taxas:
+        </p>
+
+        <p
+          style={{
+            lineHeight: "1.6rem",
+            fontSize: "0.875rem",
+          }}
+        >
+          A taxa de matrícula anual é de 1.750,00 Mt; Valor de inscrição 9.000,00 Mt. A propina de frequência é de 9.000,00 Mt por mês pagos na íntegra ou em prestações mensais durante 24 meses.
+        </p>
+
+        <p
+          style={{
+            fontSize: "0.92rem",
+            fontWeight: "bold",
+            color: "var(--main-color)",
+            lineHeight: "1.2rem",
+            marginTop: "1.5rem"
+          }}
+        >
+          Informações adicionais:
+        </p>
+
+        <p
+          style={{
+            lineHeight: "1.6rem",
+            fontSize: "0.875rem",
+          }}
+        >
+          Pederão ser obtidas na Direcção da Faculdade de Medicina, da Universidade Eduardo Mondlane. Av. Salvador Allende, n &#870; 702, CP.257, Maputo. Falar com: Sr. João Kumayo (contacto: 827536540) ou Sra. Rosita Mandlate (contacto: 828658985).
+        </p>
       </div>
     </div>
   );
