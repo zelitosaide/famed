32f29e8c71e67543bbc3e4dcb796c579c2ec0b31
zelitosaide <zelitosaide83@gmail.com>
2023-01-25 14:48:31 +0200

    Submissao de Protocolos

---
+++ src/pages/instrucoes_submissao_protocolos/InstrucoesSubmissaoProtocolos.js
@@ -0,0 +1,20 @@
+export function InstrucoesSubmissaoProtocolos() {
+  return (
+    <div style={{ paddingTop: '9.5rem' }}>
+      <div className="row">
+        <p
+          style={{
+            fontSize: '0.92rem',
+            fontWeight: 'bold',
+            color: 'var(--main-color)',
+            lineHeight: '1.2rem',
+            textTransform: 'uppercase',
+            marginBottom: '2rem',
+          }}
+        >
+          Formulário de requisição de Consulta de Bioestatística
+        </p>
+      </div>
+    </div>
+  )
+}
