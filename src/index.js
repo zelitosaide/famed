89ff1920bf3f7e599e1ffdcfae0f0b908b5d750a
zelitosaide <zelitosaide83@gmail.com>
2023-04-30 09:34:16 +0200

    Escape Hatches

--- src/index.js
+++ src/index.js
@@ -12,7 +12,6 @@ import store from './app/store'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
-  // <React.StrictMode>
   <BrowserRouter>
     <ScrollToTop>
       <Provider store={store}>
@@ -20,16 +19,11 @@ root.render(
       </Provider>
     </ScrollToTop>
   </BrowserRouter>
-  // </React.StrictMode>
 )
 
 // https://pt.finalsite.com/school-websites/custom-design-portfolio
 // https://mui.com/pt/material-ui/react-drawer/
 // https://www.aism.co.mz/
-
-
-
-
 
 // navegacao (programaticamente ativa/desativar link)
 // error handling
@@ -38,9 +32,6 @@ root.render(
 // can delete only his CV -----------------------------
 // password automatic update problema  ------------------------------------
 // form validation ----------------------------- (falta users)
-
-
-
 
 // footer
 // Team grid
