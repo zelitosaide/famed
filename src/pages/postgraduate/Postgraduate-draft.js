02a8b619fb4dd745f5b8c218a031bd0bce9f78bd
Zelito A. Saide <zelitosaide83@gmail.com>
2023-06-22 13:42:11 +0200

    Postgraduate

--- src/pages/postgraduate/Postgraduate-draft.js
+++ src/pages/postgraduate/Postgraduate-draft.js
@@ -195,6 +195,196 @@ const Postgraduate = () => {
           </div>
         </div>
 
+        <ul style={{ listStyle: 'circle', marginLeft: '1.5rem' }}>
+          <li
+            style={{
+              lineHeight: '1.6rem',
+              fontSize: '0.875rem',
+              fontWeight: 500,
+              marginBottom: 15,
+              textDecoration: 'underline',
+              textUnderlineOffset: 3,
+              textDecorationThickness: 1.5,
+              textDecorationColor: 'var(--hover-color)',
+            }}
+          >
+            {/* <Link to="msp"> */}
+            <Link to="msp">Mestrado em Saúde Pública Presencial (MSP)</Link>
+          </li>
+          <li
+            style={{
+              lineHeight: '1.6rem',
+              fontSize: '0.875rem',
+              fontWeight: 500,
+              marginBottom: 15,
+              textDecoration: 'underline',
+              textUnderlineOffset: 3,
+              textDecorationThickness: 1.5,
+              textDecorationColor: 'var(--hover-color)',
+            }}
+          >
+            {/* <Link to="mspd"> */}
+            <Link to="/postgraduate">
+              Mestrado em Saúde Pública à Distância (MSPD), com os seguintes
+              ramos:
+            </Link>
+            <ul style={{ listStyle: 'square', marginLeft: '1.5rem' }}>
+              <li
+                style={{
+                  lineHeight: '1.6rem',
+                  fontSize: '0.875rem',
+                  fontWeight: 400,
+                  marginTop: 10,
+                  textDecoration: 'underline',
+                  textUnderlineOffset: 3,
+                  textDecorationThickness: 1.5,
+                  textDecorationColor: 'white',
+                  color: 'var(--main-color)',
+                }}
+              >
+                Ramo de Desenvolvimento de Recursos Humanos para Saúde (DRHS)
+              </li>
+              <li
+                style={{
+                  lineHeight: '1.6rem',
+                  fontSize: '0.875rem',
+                  fontWeight: 400,
+                  textDecoration: 'underline',
+                  textUnderlineOffset: 3,
+                  textDecorationThickness: 1.5,
+                  textDecorationColor: 'white',
+                  color: 'var(--main-color)',
+                }}
+              >
+                Ramo de Higiene, Saúde e Segurança Ocupacional (HSSO)
+              </li>
+              <li
+                style={{
+                  lineHeight: '1.6rem',
+                  fontSize: '0.875rem',
+                  fontWeight: 400,
+                  textDecoration: 'underline',
+                  textUnderlineOffset: 3,
+                  textDecorationThickness: 1.5,
+                  textDecorationColor: 'white',
+                  color: 'var(--main-color)',
+                }}
+              >
+                Ramo em Gestão e Liderança de Sistemas de Saúde (GLSS)
+              </li>
+            </ul>
+          </li>
+          <li
+            style={{
+              lineHeight: '1.6rem',
+              fontSize: '0.875rem',
+              fontWeight: 500,
+              marginBottom: 15,
+              textDecoration: 'underline',
+              textUnderlineOffset: 3,
+              textDecorationThickness: 1.5,
+              textDecorationColor: 'var(--hover-color)',
+            }}
+          >
+            <Link to="mbc">
+              Mestrado em Biociências (MBC), com os seguintes ramos:
+            </Link>
+            <ul style={{ listStyle: 'square', marginLeft: '1.5rem' }}>
+              <li
+                style={{
+                  lineHeight: '1.6rem',
+                  fontSize: '0.875rem',
+                  fontWeight: 400,
+                  marginTop: 10,
+                  textDecoration: 'underline',
+                  textUnderlineOffset: 3,
+                  textDecorationThickness: 1.5,
+                  textDecorationColor: 'white',
+                  color: 'var(--main-color)',
+                }}
+              >
+                Ramo de Microbiologia
+              </li>
+              <li
+                style={{
+                  lineHeight: '1.6rem',
+                  fontSize: '0.875rem',
+                  fontWeight: 400,
+                  textDecoration: 'underline',
+                  textUnderlineOffset: 3,
+                  textDecorationThickness: 1.5,
+                  textDecorationColor: 'white',
+                  color: 'var(--main-color)',
+                }}
+              >
+                Ramo de Parasitologia
+              </li>
+              <li
+                style={{
+                  lineHeight: '1.6rem',
+                  fontSize: '0.875rem',
+                  fontWeight: 400,
+                  textDecoration: 'underline',
+                  textUnderlineOffset: 3,
+                  textDecorationThickness: 1.5,
+                  textDecorationColor: 'white',
+                  color: 'var(--main-color)',
+                }}
+              >
+                Ramo de Neurociências
+              </li>
+            </ul>
+          </li>
+          <li
+            style={{
+              lineHeight: '1.6rem',
+              fontSize: '0.875rem',
+              fontWeight: 500,
+              marginBottom: 15,
+              textDecoration: 'underline',
+              textUnderlineOffset: 3,
+              textDecorationThickness: 1.5,
+              textDecorationColor: 'var(--hover-color)',
+            }}
+          >
+            <Link to="mecl">
+              Mestrado em Epidemiologia de Campo e Laboratorial (MECL)
+            </Link>
+          </li>
+          <li
+            style={{
+              lineHeight: '1.6rem',
+              fontSize: '0.875rem',
+              fontWeight: 500,
+              marginBottom: 15,
+              textDecoration: 'underline',
+              textUnderlineOffset: 3,
+              textDecorationThickness: 1.5,
+              textDecorationColor: 'var(--hover-color)',
+            }}
+          >
+            <Link to="msmpi">
+              Mestrado em Saúde Mental e Psico-intervenções (MSMPI)
+            </Link>
+          </li>
+          <li
+            style={{
+              lineHeight: '1.6rem',
+              fontSize: '0.875rem',
+              fontWeight: 500,
+              marginBottom: 10,
+              textDecoration: 'underline',
+              textUnderlineOffset: 3,
+              textDecorationThickness: 1.5,
+              textDecorationColor: 'var(--hover-color)',
+            }}
+          >
+            <Link to="mepn">
+              Mestrado em Emergências Pediatrias e Neonatais (MEPN)
+            </Link>
+          </li>
+        </ul>
+
         <p
           style={{
             fontSize: '0.92rem',
