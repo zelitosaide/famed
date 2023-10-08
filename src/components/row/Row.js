5a2677ed8d718939076cdf45c6db257ec6a1a8e6
Zelito A. Saide <zelitosaide83@gmail.com>
2023-10-08 15:32:41 +0200

    Deleted

--- src/components/row/Row.js
+++
@@ -1,9 +0,0 @@
-import styles from './Row.module.css'
-
-export const Row = ({ children, ...props}) => {
-  return (
-    <div {...props} style={{ ...props.style}} className={styles.row}>
-      {children}
-    </div>
-  )
-}
--- src/pages/extension/Extension.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Extension = () => {
-  return (
-    <div>Extension</div>
-  )
-}
-
-export default Extension
