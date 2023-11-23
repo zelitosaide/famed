f61c0132d0f4e7f2141027dbb049a9098eee5efc
Zelito A. Saide <zelitosaide83@gmail.com>
2023-11-23 15:19:59 +0200

    Deploy

--- app/(website)/page.tsx
+++ app/(website)/page.tsx
@@ -1,9 +1,8 @@
-import { getNews } from "../lib/web/data";
+// import { getNews } from "../lib/web/data";
 // import Carousel from "../ui/web/carousel/carousel";
 
-export default async function Page() {
+export default function Page() {
   // const news = await getNews();
-
   return (
     <main className="pl-28 pr-28">
       <div className="mt-8">
