73046d0d6e87fddece5ab13454b657d91051f8eb
Zelito A. Saide <zelitosaide83@gmail.com>
2024-09-25 14:41:32 +0200

    More news button

--- next.config.js
+++ next.config.js
@@ -17,4 +17,22 @@ const nextConfig = {
   },
 };
 
+// remotePatterns: [
+//   {
+//     // protocol: "https",
+//     // protocol: "http",
+//     // hostname: "api.med.uem.mz",
+//     // hostname: "localhost",
+//     // port: "",
+//     // port: "3001",
+//     // pathname: "/**",
+//     // pathname: '/account123/**',
+//     //
+//     protocol: 'http',
+//     hostname: 'localhost',
+//     port: '3001',
+//     pathname: '/uploads/**',
+//   },
+// ],
+
 module.exports = nextConfig;
